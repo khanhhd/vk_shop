cf3b53ab5d4e88884e7095f740969b0d880c7cd3
khanhhd <hoang.dang.khanh@framgia.com>
2018-05-28 01:35:23 +0700

    add theme

--- vk_shop_client/src/App.js
+++ vk_shop_client/src/App.js
@@ -5,9 +5,13 @@ import { StaffList, StaffCreate, StaffEdit } from './staffs';
 import { OrderList, OrderCreate, OrderEdit } from './orders';
 import authClient from './authClient';
 
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+
 
 const App = () => (
-    <Admin restClient={jsonServerRestClient('http://localhost:3001')} authClient={authClient}>
+    <Admin theme={getMuiTheme(darkBaseTheme)}  restClient={jsonServerRestClient('http://localhost:3001')} authClient={authClient}>
         <Resource name="orders" list={OrderList} create={OrderCreate}  edit={OrderEdit}/>
         <Resource name="staffs" list={StaffList} create={StaffCreate}   />
     </Admin>
